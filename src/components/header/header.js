cbe8ac94ef6ed529bcb6c1fb32ac12ca3cb1090a
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-29 23:13:21 +0300

    Change header code
    Added dropdown hamburger menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import styles from './header.module.css'
 import find from '../../images/find.svg'
-import menu from '../../images/menu.svg'
 
 const Header = props => {
+
+    // className='active' для активных айтемов
     return (
         <header>
-            <div className={styles.header__container}>
-                <div>
-                    <a href='/home'>
-                        <h2>RANEPA CLIENT</h2>
-                    </a>
+            <div className={styles.wrapper}>
+                <div className={styles.top_navbar}>
+                    <div className={styles.hamburger}>
+                        <div className={styles.one}></div>
+                        <div className={styles.two}></div>
+                        <div className={styles.three}></div>
+                    </div>
+                    <div className={styles.top_menu}>
+                        <div className={styles.logo}>
+                            Ranepa client
+                        </div>
+                        <ul>
+                            <li><a href="/#">
+                                <img src={find} alt={'ranepa-logo'}/>
+                                <span>Найти</span>
+                            </a></li>
+                            <li><a href="/#">
+                                Профиль
+                            </a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className={styles.header__navbar}>
+
+                <div className={styles.sidebar}>
                     <ul>
-                        <a href='/sale'>
-                            <li>что-либо еще</li>
-                        </a>
-                        <a href='/profile'>
-                            <li>Профиль</li>
-                        </a>
-                        <li>
-                            <img src={find} alt='find'></img>
-                        </li>
-                        <li>
-                            <img src={menu} alt='menu'></img>
-                        </li>
+                        <li><a href="/#">
+                            <span className={styles.icon}><i className="fas fa-table"></i></span>
+                            <span className={styles.title}>Расписание</span>
+                        </a></li>
+                        <li><a href="/#">
+                            <span className={styles.icon}><i className="far fa-id-card"></i></span>
+                            <span className={styles.title}>Зачетка</span>
+                        </a></li>
+                        <li><a href="/#">
+                            <span className={styles.icon}><i className="fas fa-copy"></i></span>
+                            <span className={styles.title}>Справки</span>
+                        </a></li>
+                        <li><a href="/#">
+                            <span className={styles.icon}><i className="fas fa-users"></i></span>
+                            <span className={styles.title}>О нас</span>
+                        </a></li>
                     </ul>
                 </div>
             </div>
