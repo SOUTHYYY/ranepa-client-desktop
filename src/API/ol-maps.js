ca53959746061195776b70394f7de9afbe902b44
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-30 19:12:56 +0300

    Add middle mark

--- src/API/ol-maps.js
+++ src/API/ol-maps.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import mapboxgl from 'mapbox-gl';
-import s from  './maps.module.css';
+import s from './maps.module.css';
 // Some changes
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFmYWhlcyIsImEiOiJjazV6cW5xdDUwMDRrM21ueHF2Z3EzY3VyIn0.RRuRqnVCy3VWno0v3Xk__w';
 
@@ -34,7 +34,10 @@ export default class Application extends Component {
     render() {
         return (
             <div>
-                <div ref={el => this.mapContainer = el} className={s.mapContainer} />
+                <div className={s.mark}>
+                    <i className="fas fa-map-marker-alt"></i>
+                </div>
+                <div ref={el => this.mapContainer = el} className={s.mapContainer}/>
             </div>
         )
     }
