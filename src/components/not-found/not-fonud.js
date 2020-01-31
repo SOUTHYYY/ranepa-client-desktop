8e3d839f21b8171bd2d008066edfde044d60d341
SOUTHYYY <leshka.l1337@gmail.com>
2020-01-31 13:41:17 +0300

    Create not-found Component

---
+++ src/components/not-found/not-fonud.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import not_found from '../../images/not-found/browser.svg'
+import styles from './not-found.module.css'
+import {NavLink} from "react-router-dom";
+
+class NotFonud extends Component {
+    render() {
+        return (
+            <div className={styles.not_found}>
+                <img src={not_found}/>
+                <span>404</span>
+                <NavLink to={'/map'}>
+                    <button>
+                        Венуться на главную
+                    </button>
+                </NavLink>
+            </div>
+        );
+    }
+}
+
+export default NotFonud;
