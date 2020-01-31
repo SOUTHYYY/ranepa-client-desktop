2161655defcf85fd4b4c0f26c180ebb0d7bf1dd6
Mafahes <v.rimov@mail.ru>
2020-01-31 23:32:00 +0300

    Added Firebase config

---
+++ src/API/firebase/firebase-api.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+export const config = {
+    apiKey: "AIzaSyDpDh_wuj9NfteTWqZeNBLVmu5FIgnd4OY",
+    authDomain: "basic-lock-238415.firebaseapp.com",
+    databaseURL: "https://basic-lock-238415.firebaseio.com",
+    projectId: "basic-lock-238415",
+    storageBucket: "basic-lock-238415.appspot.com",
+    messagingSenderId: "723989397537",
+    appId: "1:723989397537:web:0fb8d658b7b8556d"
+};
+
+
