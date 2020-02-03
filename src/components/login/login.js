c379aaefe64e5e7fb586d53a90acf791f353ea8d
SOUTHYYY <leshka.l1337@gmail.com>
2020-02-04 01:31:33 +0300

    Create login component

---
+++ src/components/login/login.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+import Input from "../UI/input/input";
+import styles from './login.module.css'
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+class Login extends Component {
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный Email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                }
+            }
+        }
+    }
+    validate = (value, validation) => {
+        if (!validation) return true
+        let isValid = true
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid
+        }
+
+        return isValid
+    }
+    onChangeHandler = (event, controlName) => {
+        const formControls = {...this.state.formControls}
+        const control = {...formControls[controlName]}
+        control.value = event.target.value
+        control.touched = true
+        control.valid = this.validate(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input
+                    key={controlName + index}
+                    inpytType={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    errorMessage={control.errorMessage}
+                    shouldValidate={!!control.validation}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={styles.login}>
+                <form onSubmit={(e) => this.handleSubmit(e)} className={styles.login__form}>
+                    {this.renderInputs()}
+                    <button disabled={!this.state.isFormValid}>Войти</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
