ccd1c57875655d6945666cef5dcb39c435db91e4
SOUTHYYY <leshka.l1337@gmail.com>
2020-02-13 19:03:16 +0300

    Add new tests for Reducer

--- src/tests/reducers-test/map.spec.js
+++ src/tests/reducers-test/map.spec.js
@@ -1,5 +1,10 @@
 import {APIReducer, initialState} from "../../redux/reducers/api_reducer";
-import { FETCH_API_START, FETCH_API_SUCCESS, FETCH_API_FAILURE} from '../../redux/actions/action_types';
+import {
+    FETCH_API_START,
+    FETCH_API_SUCCESS,
+    FETCH_API_FAILURE,
+    FETCH_USER_PINS_START, FETCH_USER_PINS_SUCCES, FETCH_USER_PINS_FAILURE
+} from '../../redux/actions/action_types';
 
 
 
@@ -44,6 +49,46 @@ describe('APIReducer test', () => {
             ...state,
             loading: false,
             data: action.payload
+        })
+    })
+    it ('FETCH_USER_PINS_START TEST', () => {
+        const action = {
+            type: FETCH_USER_PINS_START,
+        }
+        expect(APIReducer(initialState, action)).toEqual({
+            ...initialState,
+            loading: true,
+            errorMessage: null
+        })
+    })
+    it ('FETCH_USER_PINS_SUCCES TEST', () => {
+        const state = {
+            ...initialState,
+            loading: true,
+        }
+        const action = {
+            type: FETCH_USER_PINS_SUCCES,
+            payload: [{id: 1, data: 123123}, {id: 1, data: 123123}, {id: 1, data: 123123}]
+        }
+        expect(APIReducer(state, action)).toEqual({
+            ...state,
+            loading: false,
+            userPins: action.payload
+        })
+    })
+    it ('FETCH_USER_PINS_FAILURE TEST', () => {
+        const state = {
+            ...initialState,
+            loading: true,
+        }
+        const action = {
+            type: FETCH_USER_PINS_FAILURE,
+            payload: 'Some error'
+        }
+        expect(APIReducer(state, action)).toEqual({
+            ...state,
+            loading: false,
+            errorMessage: action.payload
         })
     })
 })
